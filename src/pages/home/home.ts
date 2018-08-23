8c196cd293ec9700d3f325f12ae8c12f435cad07
xyz607xx <xyz607xx@gmail.com>
2018-08-24 00:56:59 +0800

    show word category

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { File } from "@ionic-native/File";
+import { Http } from "@angular/http";
+import "rxjs";
+
+import { WordPage } from "../word/word";
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  categories: any
 
+  constructor(
+    public navCtrl: NavController,
+    public file: File,
+    public http: Http,
+  ) {
+    this.http.get('assets/data/category.json').map(res => res.json()).subscribe(data => {
+      this.categories = data;
+    },
+      err => {
+        alert(err);
+      });
+  }
+
+  nextPage(category) {
+    this.navCtrl.push(WordPage, category);
   }
 
 }
