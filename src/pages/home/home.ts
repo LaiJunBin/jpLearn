774bc85091b7fb4aeabda83aeb750987b4cfcf2b
xyz607xx <xyz607xx@gmail.com>
2018-08-28 22:24:41 +0800

    設備上寫檔改以遞迴方式

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -29,35 +29,38 @@ export class HomePage {
 
     this.http.get(`assets/data/word.json`).map(res => res.json()).subscribe(data => {
 
+      //PC Test
 
-      let keys = Object.keys(data);
-      this.words = keys.map(function (key) {
-        let obj = data[key];
-        obj.title = key;
-        obj.remark = obj.remark || false;
-        return obj;
-      });
-
-
-      // this.file.checkDir(this.file.dataDirectory, 'words').then(res => {
-      //   this.readWords();
-      // }, err => {
-      //   this.file.createDir(this.file.dataDirectory, 'words', false).then(res => {
-      //     let count = 0;
-      //     Object.keys(data).forEach((key, i) => {
-      //       this.file.writeFile(this.file.dataDirectory + '/words/', key + '.json', JSON.stringify(data[key])).then(res => {
-      //         count++;
-      //         if (count == Object.keys(data).length - 1) {
-      //           this.readWords();
-      //         }
-      //       }, err => {
-      //         alert('檔案寫入失敗!');
-      //       })
-      //     });
-      //   }, err => {
-      //     alert('目錄建立失敗');
-      //   });
+      // let keys = Object.keys(data);
+      // this.words = keys.map(function (key) {
+      //   let obj = data[key];
+      //   obj.title = key;
+      //   obj.remark = obj.remark || false;
+      //   return obj;
       // });
+
+
+      //Device Test
+
+      this.file.checkDir(this.file.dataDirectory, 'words').then(res => {
+        this.readWords();
+      }, err => {
+        this.file.createDir(this.file.dataDirectory, 'words', false).then(res => {
+          let count = 0;
+          Object.keys(data).forEach((key, i) => {
+            this.file.writeFile(this.file.dataDirectory + '/words/', key + '.json', JSON.stringify(data[key])).then(res => {
+              count++;
+              if (count == Object.keys(data).length - 1) {
+                this.readWords();
+              }
+            }, err => {
+              alert('檔案寫入失敗!');
+            })
+          });
+        }, err => {
+          alert('目錄建立失敗');
+        });
+      });
 
     }, err => {
       alert(err);
@@ -80,26 +83,29 @@ export class HomePage {
 
   readWords() {
     this.file.listDir(this.file.dataDirectory, 'words').then(files => {
-      files.map(file => {
+      this._readWord(files.map(file => {
         return file.name;
       }).sort(function compare(n1, n2) {
         return n1.localeCompare(n2, "zh-Hant");
-      }).forEach(file => {
-        this.file.readAsText(this.file.dataDirectory + '/words/', file).then(res => {
-          let obj = JSON.parse(res);
-          obj.title = file.replace('.json', '');
-          obj.remark = obj.remark || false;
-          this.words.push(obj);
-        }, err => {
-          alert('單字讀取失敗!');
-        });
-      });
-
-
+      }));
     }, err => {
       alert("目錄讀取失敗");
     })
 
+  }
+
+  _readWord(files, n = 0) {
+    if (n >= files.length) return;
+    let file = files[n];
+    this.file.readAsText(this.file.dataDirectory + '/words/', file).then(res => {
+      let obj = JSON.parse(res);
+      obj.title = file.replace('.json', '');
+      obj.remark = obj.remark || false;
+      this.words.push(obj);
+      this._readWord(files, n + 1);
+    }, err => {
+      alert('單字讀取失敗!');
+    });
   }
 
 
