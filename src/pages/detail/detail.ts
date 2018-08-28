8a7cc8e7e5c22d6a3c1a285ebba51eb92b449912
xyz607xx <xyz607xx@gmail.com>
2018-08-28 23:18:17 +0800

    長按單字可修改&刪除

--- src/pages/detail/detail.ts
+++ src/pages/detail/detail.ts
@@ -115,4 +115,138 @@ export class DetailPage {
     }).present();
   }
 
+  longPress(index) {
+    if (this.word[this.pronunciation][index].sound !== undefined) {
+      this.alertCtrl.create({
+        title: '提示',
+        message: '無法變更內建單字',
+        buttons: ['OK!']
+      }).present();
+      return;
+    }
+
+    this.alertCtrl.create({
+      title: '操作',
+      buttons: [
+        {
+          text: '變更',
+          handler: _ => {
+            this.alertCtrl.create({
+              title: '選取操作',
+              buttons: [
+                {
+                  text: '刪除',
+                  handler: _ => {
+                    this.alertCtrl.create({
+                      title: '提示',
+                      message: '確定要刪除嗎? 刪除後無法復原',
+                      buttons: [
+                        {
+                          text: '確認',
+                          handler: _ => {
+                            this.file.readAsText(this.file.dataDirectory + '/words/', this.word.title + '.json').then(res => {
+                              let word = JSON.parse(res);
+
+                              word[this.pronunciation].splice(index, 1);
+
+                              this.file.writeExistingFile(this.file.dataDirectory + '/words/', this.word.title + '.json', JSON.stringify(word)).then(res => {
+                                this.word[this.pronunciation].splice(index, 1);
+                                this.alertCtrl.create({
+                                  title: '提示',
+                                  message: '刪除成功',
+                                  buttons: ['OK!']
+                                }).present();
+                              }, err => {
+                                this.alertCtrl.create({
+                                  title: '提示',
+                                  message: '刪除失敗',
+                                  buttons: ['OK!']
+                                }).present();
+                              });
+                            }, err => {
+                              alert('讀取檔案失敗');
+                            });
+                          }
+                        }, {
+                          text: 'Cancel',
+                          role: 'cancel',
+                        }
+                      ]
+                    }).present();
+                  }
+                },
+                {
+                  text: '修改',
+                  handler: _ => {
+                    this.alertCtrl.create({
+                      title: '修改',
+                      inputs: [
+                        {
+                          name: 'cn',
+                          value: this.word[this.pronunciation][index].cn,
+                          placeholder: '輸入中文'
+                        },
+                        {
+                          name: 'jp',
+                          value: this.word[this.pronunciation][index].jp,
+                          placeholder: '輸入日文',
+                        }
+                      ],
+                      buttons: [
+                        {
+                          text: '確認修改',
+                          handler: data => {
+                            if (data.cn == "" || data.jp == "") {
+                              this.alertCtrl.create({
+                                title: '提示',
+                                message: "請確定所有欄位都有輸入",
+                                buttons: ['OK!']
+                              }).present();
+                              return false;
+                            }
+
+                            this.file.readAsText(this.file.dataDirectory + '/words/', this.word.title + '.json').then(res => {
+                              let word = JSON.parse(res);
+                              word[this.pronunciation][index].cn = data.cn;
+                              word[this.pronunciation][index].jp = data.jp;
+
+                              this.file.writeExistingFile(this.file.dataDirectory + '/words/', this.word.title + '.json', JSON.stringify(word)).then(res => {
+                                this.word[this.pronunciation][index].cn = data.cn;
+                                this.word[this.pronunciation][index].jp = data.jp;
+                                this.alertCtrl.create({
+                                  title: '提示',
+                                  message: '修改成功',
+                                  buttons: ['OK!']
+                                }).present();
+                              }, err => {
+                                this.alertCtrl.create({
+                                  title: '提示',
+                                  message: '修改失敗',
+                                  buttons: ['OK!']
+                                }).present();
+                              });
+                            }, err => {
+                              alert('讀取檔案失敗');
+                            });
+                          }
+                        }, {
+                          text: 'Cancel',
+                          role: 'cancel',
+                        }
+                      ]
+                    }).present();
+                  }
+                }
+              ]
+            }).present();
+          }
+        }, {
+          text: 'Cancel',
+          role: 'cancel',
+        }
+      ]
+    }).present();
+
+  }
+
 }
